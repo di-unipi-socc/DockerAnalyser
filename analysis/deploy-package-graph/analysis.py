91d2aa816c87c761e27e1ea9e8075262b4e2c8c8
dido18 <davide.neri@di.unipi.it>
2017-06-05 11:00:33 +0200

    code refacroting of DOCKER GRAPH

--- analysis/deploy-package-graph/analysis.py
+++ analysis/deploy-package-graph/analysis.py
@@ -1,80 +1,95 @@
 import requests
 from lxml import html
-import sys
-
 import re
 
 
-def on_message( image, context):
+def on_message(image, context):
     # {"star_count": 1177, "pull_count": 20511731, "repo_owner": null,
-    # "short_description": "GitLab Community Edition docker image based on the Omnibus package",
-    # "is_automated": true, "is_official": false, "repo_name": "gitlab/gitlab-ce",
-    # "tag": "nightly", "name": "gitlab/gitlab-ce:nightly", "full_size": 412857400,
-    # "images": [{"size": 412857400, "architecture": "amd64", "variant": null,
-    # "features": null, "os": "linux", "os_version": null, "os_features": null}],
-    # "id": 4253706, "repository": 252439, "creator": 454693, "last_updater": 454693,
-    # "last_updated": "2017-05-31T02:06:25.100551Z", "image_id": null, "v2": true}
+    # "short_description": "GitLab Comm",
+    # "is_automated": true, "is_official": false,
+    # "repo_name": "gitlab/gitlab-ce",
+    # "tag": "nightly", "name": "gitlab/gitlab-ce:nightly",
+    # "full_size": 412857400
 
     logger = context['logger']
-    client_images = context['images']
+    # client_images = context['images']
 
     repo = image["repo_name"]
-
     logger.info("Received image to be analysed: {} ".format(repo))
+    if not is_stored_locally(repo):
+        # image is not present into local database
+        logger.info("{} is not present into local database} ".format(repo))
+        # image dictionary with the field to be sent to the images_server
+        node_image = {'name': repo}
+        dockerfile = get_dockerfile(repo)
+        from_repo, from_tag = extract_FROM(dockerfile)
+        logger.info("{} FROM {} {}"
+                    .format(reo, from_repo,
+                            "" if from_tag is None else from_tag))
 
-    r = requests.get("http://images_server:3000/api/images?name={}".format(repo)) #http://127.0.0.1:3000/api/images?page=1&limit=100&name=sameersbn/gitlab
-
-    if r.json()['count'] == 0:
-        node_image = dict()
-        node_image['name'] = repo
-
-        from_image = parse_dockerfile(get_dockerfile(repo))
-        logger.info("From image extracted {} ".format(from_image))
-        if ":" in from_image:
-            from_repo, from_tag = from_image.split(":")
-        else:
-            from_repo = from_image
-            from_tag = None
         node_image['from_repo'] = from_repo
         node_image['from_tag'] = from_tag
         node_image["is_official"] = image["is_official"]
         node_image["is_automated"] = image["is_automated"]
-        logger.info("Push {}".format(node_image))
-
+        logger.info("POST {} ".format(node_image))
         client_images.post_image(node_image)
-
         return True
     else:
         logger.info("{}  already present into local database ".format(repo))
         return True
 
-# {"contents": "FROM python:2.7\n\n# FileAuthor /Maintaner\nMAINTAINER Davide
-# Neri\n\nENV PYTHONUNBUFFERED 1\nRUN mkdir /code\nWORKDIR /code\nADD requirements.txt <
-# /code/\nRUN pip install -r requirements.txt\nADD . /code/\n"}
+
+def is_stored_locally(repo_name):
+    """
+    Return True if the *repo_name** is stored into *images_server*,
+    else return False.
+    Example:
+    repo name: *sameersbn/gitlab*:
+        GET http://127.0.0.1:3000/api/images?name=sameersbn/gitlab
+    """
+    try:
+        response = requests.get("http://images_server:3000/api/images?name={}"
+                                .format(repo))
+        return True if response.json()['count'] == 0 else False
+    except ConnectionError as e:
+        logger.error(str(e))
+        raise
 
 
-# GITHUB official library
-# https://github.com/docker-library/official-images/tree/master/library
-# https://raw.githubusercontent.com/docker-library/official-images/master/library/[java]
+def extract_FROM(dockerfile):
+    from_image = re.search('FROM ([^\s]+)', dockerfile).group(1)
+    if ":" in from_image:  # FROM  reponame:tag
+        from_repo, from_tag = from_image.split(":")
+    else:                  # FROM reponame
+        from_repo = from_image
+        from_tag = None
+    return from_repo, from_tag
 
-def parse_dockerfile(dockerfile):
-    return re.search('FROM ([^\s]+)', dockerfile).group(1)
+
+def get_dockerfile(repo_name):
+    #  https://hub.docker.com/v2/repositories/dido/webofficina/dockerfile/
+    #  https://hub.docker.com/v2/repositories/kaggle/python/dockerfile/
+    docker_url = "https://hub.docker.com/v2/repositories/{}/dockerfile/"
+    logger.info(docker_url.format(repo_name))
+    response = requests.get(docker_url.format(repo_name))
+    dockerfile = response.json()['contents']
+    return dockerfile
 
 
-github_url = "https://github.com/"
+def get_officials_FROM():
+    # GITHUB official library
+    # https://github.com/docker-library/official-images/tree/master/library
+    # https://raw.githubusercontent.com/docker-library/official-images/master/library/[java]
 
+    github_url = "https://github.com/"
 
-def get_officials():
-    page = requests.get(
-        "https://github.com/docker-library/official-images/tree/master/library")
-
+    page = requests.get(github_url +
+                        "docker-library/official-images/tree/master/library")
     tree = html.fromstring(page.content)
 
-    # return the list of name of the official images [''adminer', 'aerospike',
+    # list of names of the official images [''adminer', 'aerospike',..]
     name_off = tree.xpath('//td[@class="content"]/span/a/text()')
-
     node = dict()
-
     for name in name_off:
         # retrive the url path to the github library official e.g.
         # ['/docker-library/official-images/blob/master/library/adminer']
@@ -102,7 +117,7 @@ def get_officials():
                 "//tr[td[contains(text(), 'Directory')]]/*/text()")
             # githubrepo_tags = tree_github.xpath("//tr[td[contains(text(), 'Directory')]]/*/text()")
             dirs = [d.split(":")[1].strip()
-                    for d in githubrepo_dirs]                         # i.strip()
+                    for d in githubrepo_dirs]    # i.strip()
             node["Directory"] = dirs
 
             for directory in node["Directory"]:
@@ -121,6 +136,7 @@ def get_officials():
                 from_image = parse_dockerfile(dockerfile)
                 print("Base image {} ".format(from_image))
                 node['from_image'] = from_image
+                yield node
         else:
             print("GitRepo has not been found")
 
@@ -128,13 +144,5 @@ def get_officials():
     # https://raw.githubusercontent.com/tianon/docker-bash/master/4.3/Dockerfile
 
 
-def get_dockerfile(repo_name):
-    docker_url = "https://hub.docker.com/v2/repositories/{}/dockerfile/"
-    #  https://hub.docker.com/v2/repositories/dido/webofficina/dockerfile/
-    #  https://hub.docker.com/v2/repositories/kaggle/python/dockerfile/
-    response = requests.get(docker_url.format(repo_name))
-    dockerfile = response.json()['contents']
-    return dockerfile
-
-#print(getDockerfile("dido/webofficina"))
-#get_officials()
+# for image in get_officials_FROM():
+#     print(image)
